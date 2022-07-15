1c57103f3aaa34aa443b652cda34c9ed674eacec
pradhyankita <rapenzelpb@gmail.com>
2022-07-15 10:18:05 +0530

    adding: UC7

--- src/com/bridgelabz/snake_ladder.java
+++ src/com/bridgelabz/snake_ladder.java
@@ -1,53 +1,94 @@
 package com.bridgelabz;
 
 public class snake_ladder {
-    static int position = 0;
-
-    public enum Option {
-        NO_PLAY, SNAKE, LADDER
-    }
-
-    static final int NO_PLAY = 1;
-    static final int SNAKE = 2;
-    static final int LADDER = 3;
-    static final int WINNING_POSITION = 100;
-    static final int INITIAL_POSITION = 0;
-    static int dieCount;
-    static int dieRoll() {
-        int dice = (int) (Math.random() * 10) % 6 + 1;
-        dieCount++;
-        return dice;
-
-    }
-
-    static int getOption() {
-        return (int) (Math.random() * 10) % 3 + 1;
-    }
+    static final int StART_POSITION = 0;
+    static final int LADDER_OPTION = 2;
+    static final int SNAKE_OPTION = 1;
+    static final int PLAYER_1 = 1;
+    static final int PLAYER_2 = 2;
 
     public static void main(String[] args) {
-        while (position < WINNING_POSITION) {
-            int dieNum = dieRoll();
-            int option = getOption();
-            switch (option) {
-                case SNAKE:
-                    position -= dieNum;
-                    if(position<INITIAL_POSITION){
-                        position=INITIAL_POSITION;
-                    }
-                    break;
-                case LADDER:
-                    //position += dieNum;
-                    if(position+dieNum < WINNING_POSITION){
-                        position+=dieNum;
+        int dieNumber, option;
+        int dieCount = 0;
+        int player1Position = StART_POSITION;
+        int player2Position = StART_POSITION;
+        int currentPlayer = PLAYER_1;
 
+
+        while (player1Position < 100 && player2Position < 100) {
+            dieNumber = rollDie();
+            dieCount++;
+            System.out.println("#" + dieCount);
+            System.out.println("Die Number = " + dieNumber);
+
+            option = checkOption();
+
+            switch (option) {
+                case LADDER_OPTION:
+                    System.out.println("ladder option");
+                    if (currentPlayer == PLAYER_1) {
+                        player1Position += dieNumber;
+                    } else {
+                        player2Position += dieNumber;
                     }
                     break;
-                case NO_PLAY:
+                case SNAKE_OPTION:
+                    System.out.println("snake option");
+                    if (currentPlayer == PLAYER_1) {
+                        player1Position -= dieNumber;
+                    } else {
+                        player2Position -= dieNumber;
+                    }
+                    currentPlayer = switchPlayer(currentPlayer);
                     break;
                 default:
-
+                    System.out.println("No Play option");
+                    currentPlayer = switchPlayer(currentPlayer);
+                    break;
             }
-            System.out.println("Current position= "+position);
+
+            //ensure start position and exact winning position
+            if (player1Position < 0) {
+                player1Position = 0;
+            } else if (player2Position < 0) {
+                player2Position = 0;
+            } else if (player1Position > 100) {
+                player1Position -= dieNumber;
+            } else if (player2Position > 100) {
+                player2Position -= dieNumber;
+            }
+
+            System.out.println("current player1 position : " + player1Position);
+            System.out.println("current player2 position : " + player2Position);
+
+
+        }
+        System.out.println("***********");
+        showWinner(player1Position, player2Position);
+        System.out.println("Number of times die played = " + dieCount);
+    }
+
+    static int rollDie() {
+        return (int) (Math.random() * 10 % 6);
+    }
+
+    static int checkOption() {
+        return (int) (Math.random() * 10 % 3);
+    }
+
+    static int switchPlayer(int currentPlayer) {
+        if (currentPlayer == PLAYER_1) {
+            return PLAYER_2;
+        } else {
+            return PLAYER_1;
+        }
+    }
+
+    static void showWinner(int player1Position, int player2Position) {
+        if (player1Position == 100) {
+            System.out.println("Player1 is Winner");
+        } else if (player2Position == 100) {
+            System.out.println("Player2 is Winner");
         }
     }
 }
