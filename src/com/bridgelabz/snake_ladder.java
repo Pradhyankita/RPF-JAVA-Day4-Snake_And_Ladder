939610b2b9494fdf59a746f5126bc47aed4ca1db
pradhyankita <rapenzelpb@gmail.com>
2022-07-15 09:45:05 +0530

    adding: UC3

--- src/com/bridgelabz/snake_ladder.java
+++ src/com/bridgelabz/snake_ladder.java
@@ -2,6 +2,12 @@ package com.bridgelabz;
 
 public class snake_ladder {
     static int position=0;
+    public enum Option{
+        NO_PLAY,SNAKE,LADDER
+    }
+    static final int NO_PLAY=1;
+    static final int SNAKE=2;
+    static final int LADDER=3;
     static int dieRoll(){
         int dice =(int) (Math.random() *10)%6+1;
         return dice;
@@ -13,6 +19,18 @@ public class snake_ladder {
 
         int dieNum=dieRoll();
        int  option=getOption();
+       switch(option){
+           case SNAKE:
+               position-=dieNum;
+               break;
+           case LADDER:
+               position+=dieNum;
+               break;
+           case NO_PLAY:
+               break;
+           default:
+
+       }
 
     }
 }
